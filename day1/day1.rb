a4d2bfbf00508209e39ad98ac14b93d96ca54036
Youssef Gazzar <uosefgazzar@gmail.com>
2023-12-02 12:29:27 +0200

    Handle edge cases

--- day1/day1.rb
+++ day1/day1.rb
@@ -14,6 +14,15 @@ nums = {
   'seven'=> '7',
   'eight'=> '8',
   'nine'=> '9',
+  'eno'=> '1',
+  'owt'=> '2',
+  'eerht'=> '3',
+  'ruof'=> '4',
+  'evif'=> '5',
+  'xis'=> '6',
+  'neves'=> '7',
+  'thgie'=> '8',
+  'enin'=> '9',
   '1' => '1',
   '2' => '2',
   '3' => '3',
@@ -25,15 +34,20 @@ nums = {
   '9' => '9'
 }
 
-
-
 # Part 1
-# result = data.map { _1.scan(/\d/).values_at(0, -1).join.to_i }.sum
+# result = data.map { _1.scan(/\d/).values_at(0, -1).join.to_i }
 
 # Part 2
-result = data.map { |line| line.scan(/(\d|one|two|three|four|five|six|seven|eight|nine)/).values_at(0, -1).flatten.map { nums[_1] }.join.to_i }.sum
+forward_keys = Regexp.union(nums.keys)
+backward_keys = Regexp.union(nums.keys.map(&:reverse))
 
-puts result
+result = data.map do |line| 
+  first = line.scan(forward_keys).first
+  last = line.reverse.scan(backward_keys).first
+  (nums[first] + nums[last]).to_i
+end
+
+puts result.sum
 
 __END__
 two1nine
