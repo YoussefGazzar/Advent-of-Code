faa99f539d01117401514cadeae4f89c97235b96
Youssef Gazzar <uosefgazzar@gmail.com>
2023-12-11 13:58:50 +0200

    Part 2

--- day8/day8.rb
+++ day8/day8.rb
@@ -13,17 +13,46 @@ graph = graph.each_with_object({}) do |direction, hash|
   hash[match[:key]] = {'L'=> match[:l], 'R'=> match[:r]}
 end
 
-current = 'AAA'
-directions.each_with_index do |direction, i|
-  return pp i+1 if graph[current][direction] == 'ZZZ'
-  current = graph[current][direction]
+# Part 1
+# current = 'AAA'
+# directions.each_with_index do |direction, i|
+#   return pp i+1 if graph[current][direction] == 'ZZZ'
+#   current = graph[current][direction]
+# end
+
+current_nodes = graph.keys.select {_1.end_with?('A')}
+pp current_nodes
+
+
+cycles = current_nodes.each_with_object([]) do |node, arr|
+  directions.each_with_index do |direction, i|
+    if node.end_with?('Z')
+      pp "Node: #{node}, I: #{i}"
+      arr << i
+      break
+    end
+    node = graph[node][direction]
+  end
 end
 
+pp cycles
+pp cycles.reduce(1, :lcm)
 
+# "Node: DDZ, I: 20093"
+# "Node: XDZ, I: 12169"
+# "Node: ZZZ, I: 22357"
+# "Node: SRZ, I: 14999"
+# "Node: JVZ, I: 13301"
+# "Node: THZ, I: 17263"
 
 __END__
-LLR
+LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
