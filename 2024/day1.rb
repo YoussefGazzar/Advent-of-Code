c8d5cf0b71821fa464ed57bc84cc5b2c4834491e
Youssef Gazzar <uosefgazzar@gmail.com>
2024-12-01 17:43:02 +0200

    Update my template to use input_fetcher

--- 2024/day1.rb
+++ 2024/day1.rb
@@ -1,23 +1,20 @@
-require 'dotenv/load'
-require 'open-uri'
-url = "https://adventofcode.com/2024/day/1/input"
+require_relative '../fetch_input'
 
-URI.open(url, "Cookie" => "session=#{ENV['COOKIE_SESSION']}") {|f|
-  # # part 1  
-  # p f.readlines(chomp: true)
-  # .map(&:split)
-  # .map { |x| x.map(&:to_i) }
-  # .transpose
-  # .map(&:sort)
-  # .transpose
-  # .map { |x| (x[-1] - x[0]).abs }
-  # .sum
- 
-  # part 2
-  h1, h2 = f.readlines(chomp: true)
-  .map(&:split)
-  .map { |x| x.map(&:to_i) }
-  .transpose
-  .map(&:tally)
-  p h1.reduce(0)  { |sum, (k, v)| sum + v * k * h2[k].to_i }
-}
+input = InputFetcher.fetch(2024, 1)
+# part 1  
+p input
+.map(&:split)
+.map { |x| x.map(&:to_i) }
+.transpose
+.map(&:sort)
+.transpose
+.map { |x| (x[-1] - x[0]).abs }
+.sum
+
+# part 2
+h1, h2 = input
+.map(&:split)
+.map { |x| x.map(&:to_i) }
+.transpose
+.map(&:tally)
+p h1.reduce(0) { |sum, (k, v)| sum + v * k * h2[k].to_i }
