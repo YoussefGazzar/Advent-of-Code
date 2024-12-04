757a9926223ee30dbc6de84cc01f351eab06cea5
Youssef Gazzar <uosefgazzar@gmail.com>
2024-12-04 18:51:38 +0200

    Add fetching as a bulk

--- fetch_input.rb
+++ fetch_input.rb
@@ -2,9 +2,12 @@ require 'dotenv/load'
 require 'open-uri'
 
 class InputFetcher
-  def self.fetch(year, day)
+  def self.fetch(year, day, bulk = false)
     url = "https://adventofcode.com/#{year}/day/#{day}/input"
-    input = URI.open(url, "Cookie" => "session=#{ENV['COOKIE_SESSION']}") { |f| f.readlines(chomp: true) }
+    URI.open(url, "Cookie" => "session=#{ENV['COOKIE_SESSION']}") do |f|
+      return f.read if bulk
+      f.readlines(chomp: true)
+    end
   rescue OpenURI::HTTPError => e
     puts "Failed to fetch input for #{year} day #{day}: #{e.message}"
     []
